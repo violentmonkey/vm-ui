067d9185072382652186b34ceda9d0077f7c0274
Gerald <gera2ld@live.com>
2021-03-18 16:14:24 +0800

    chore: upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: [
     require.resolve('@gera2ld/plaid-common-ts/eslint'),
+    require.resolve('@gera2ld/plaid-common-react/eslint'),
   ],
   parserOptions: {
     project: './tsconfig.json',
